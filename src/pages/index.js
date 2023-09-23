274b7a1e6de642377529e13fef7600e62fd24911
monkeymr014 <monkeymr014@gmail.com>
2023-09-23 17:28:34 +0200

    add text and foto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,35 @@
-import { Text, Box } from '@chakra-ui/react'
+import { Highlight, Box, Image, useColorModeValue, Flex, Center } from '@chakra-ui/react'
 import Technology from '@/components/technology'
 export default function Home() {
   return (
     <Box mt={20} align="center">
-      <Text fontSize="2xl">Cześć, jestem ludzikiem który uczy się kodować.</Text>
+      <Flex m={2}>
+        <Box flex="1">
+          <Highlight
+            query="Cześć, jestem Michał"
+            styles={{
+              px: '2',
+              py: '1',
+              rounded: 'full',
+              fontSize: '20px',
+              bgGradient: useColorModeValue('linear(to-l,red.500, yellow.500)', 'linear(to-l, #03b5aa, #dbfe87)'),
+            }}
+          >
+            Cześć, jestem Michał, początkujący frontendowiec, pasjonat kodowania. Kiedy nie jestem zakopany w kodzie, po prostu odprężam się na
+            perkusji - rytm to moje drugie imię. A gdy tylko mogę, uciekam od monitora, by cieszyć się przyrodą.
+          </Highlight>
+        </Box>
+        <Center w="120px" ml={5}>
+          <Image
+            src="./foto.jpg"
+            border="solid"
+            boxSize="120px"
+            borderWidth="2px"
+            borderColor={useColorModeValue('sunset.500', 'moonstone.500')}
+            borderRadius="full"
+          />
+        </Center>
+      </Flex>
       <Technology />
     </Box>
   )
